f77847adbfc7ecdc043f545683cfddb19411821b
Rich Chau <richsensations@gmail.com>
2021-10-14 08:21:18 -0700

    Router for Game Page

---
+++ src/frontend/src/pages/GamePage.js
@@ -0,0 +1,13 @@
+import {React, useEffect, useState} from 'react';
+import { useParams } from 'react-router-dom';
+import { GameDetailCard } from '../components/GameDetailCard';
+import { GameSmallCard } from '../components/GameSmallCard';
+
+export const GamePage = () => {
+
+    return (
+        <div className="GamePage">
+        <h1>Match Page</h1>
+        </div>
+    );
+}
